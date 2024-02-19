63a02eb2077b8c3b3f3495c8a8d51f2d64a75410
Simeon Simeonov <s.simeonoff@gmail.com>
2024-02-19 19:02:27 +0100

    remove default label

--- src/containers/Main.tsx
+++ src/containers/Main.tsx
@@ -1,16 +1,14 @@
 import Galleries from "../components/Galleries";
 import ImagesGrid from "./Images";
-import { ResultLabel } from "../components/styles";
 import { useFlickrContext } from "../contexts/FlickrContext";
 
 const Main: React.FC = () => {
-  const { data, error, searchedText } = useFlickrContext();
+  const { data, error } = useFlickrContext();
 
   return (
     <>
       <Galleries />
       {data ? <ImagesGrid /> : error}
-      {!searchedText && <ResultLabel>Search for images</ResultLabel>}
     </>
   );
 };
